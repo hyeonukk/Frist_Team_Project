8d495daac13bf5a36e0eb9c450ced5473c994623
abcam <abcam@LAPTOP-6E84OO5M>
2022-11-04 16:52:40 +0900

    Team.Project.Version2

---
+++ Project/src/Kiosk/Version2/Lucky.java
@@ -0,0 +1,19 @@
+package Kiosk.Version2;
+
+public class Lucky {
+
+    public static void lotto() {
+
+         System.out.println("====================");
+         System.out.println("추첨을 돌리겠습니다!");
+
+         int luckyAnswer = (int) (Math.random() *20) +1;
+         if(Binfo.luck ==  luckyAnswer) {
+            System.out.println("당첨되었습니다!!");
+         }else {
+            System.out.println("꽝입니다");
+         }
+         System.out.println("====================");
+      }
+
+   }
---
+++ Project/src/Kiosk/Version2/MainClass.java
@@ -0,0 +1,13 @@
+package Kiosk.Version2;
+
+public class MainClass {
+
+   public static void main(String[] args) {
+      Admin ad = new Admin();
+      
+      ad.admin();
+      
+      
+   }
+
+}
